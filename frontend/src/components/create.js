993db6f69242f1c7c18521a0d09b3ca9a8db4ef6
Andy Wu <andyww09@gmail.com>
2023-11-07 21:40:47 -0800

    add handle submit and reset

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -4,22 +4,27 @@ import { useState, } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Create() {
-    const { register, handleSubmit } = useForm()
-    const onSubmit = (data, e) => console.log(data, e)
+    const { register, handleSubmit, reset } = useForm()
     const onError = (errors, e) => console.log(errors, e)
 
-    function saveUser() {
-        UserDataService.create()
+    function onSubmit(data, e){
+        console.log(data, e)
+        saveUser(data);
+        reset();
     }
+
+    function saveUser(data) {
+        UserDataService.create(data);
+    }
+
 
 
     return (
         <div>
             <h1>Create</h1>
-            <form onSubmit={(onSubmit, onError)}>
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
                 <label>First Name</label>
                 <input {...register("firstName")} />
-      
                 <label>Last Name</label>
                 <input {...register("lastName")} />
                 <button type="submit">Submit</button>
