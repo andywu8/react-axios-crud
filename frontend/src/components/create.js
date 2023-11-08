9f961bcf39202d4cc83da9a7c941ebcf13350064
Andy Wu <andyww09@gmail.com>
2023-11-07 19:31:43 -0800

    Add submission form

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -1,10 +1,29 @@
 import React from "react";
-import User
+import UserDataService from "../services/user.service";
+import { useState, } from "react";
+import { useForm } from "react-hook-form";
+
 export default function Create() {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data, e) => console.log(data, e)
+    const onError = (errors, e) => console.log(errors, e)
+
+    function saveUser() {
+        UserDataService.create()
+    }
+
+
     return (
         <div>
             <h1>Create</h1>
-            <input></input>
+            <form onSubmit={(onSubmit, onError)}>
+                <label>First Name</label>
+                <input {...register("firstName")} />
+      
+                <label>Last Name</label>
+                <input {...register("lastName")} />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -11,5 +11,4 @@ class UserDataService {
 
 
 }
-
 export default new UserDataService();
