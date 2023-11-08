d871bc0d669dbc46f53d2e9a7e1ec9d2900cf958
Andy Wu <andyww09@gmail.com>
2023-11-07 18:20:43 -0800

    create, read, update components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Create from './components/Create';
+import Read from './components/Read';
+import Update from './components/Update';
 
 function App() {
   return (
-    <div className="main">
-      <h2 className="main-header">
+    <>
+      <div>
         React Crud App
-      </h2>
-    </div>
+      </div>
+      <Create />
+      <Read />
+      <Update />
+    </>
   );
 }
 
---
+++ frontend/src/components/create.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Create() {
+    return (
+        <div>
+            <h1>Create</h1>
+            <input></input>
+        </div>
+    )
+}
