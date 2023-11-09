11844c0c008cbdc3c46fcb2ab8ac27f8f86973c6
Andy Wu <andyww09@gmail.com>
2023-11-08 16:46:12 -0800

    add retrieve users to list

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import UserDataService from '../services/user.service';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function List() {
+    const [users, setUsers] = React.useState([]);
+
+    function retrieveUsers(){
+        UserDataService.getAll()
+        .then((response) => {
+            setUsers(response.data);
+            console.log(response.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    }
     return (
         <>
             <div>
                 <h1>List</h1>
+                {users.map((user) => (<li>{user}</li>))}
             </div>
         </>
     )
